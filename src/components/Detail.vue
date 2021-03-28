15695bed0c1ec2abfecf54689d0dcf966fae2dea
junhochoi <braverokmc79@gmail.com>
2021-03-28 22:46:55 +0900

    게시판 만들기

---
+++ src/components/Detail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>{{data.writer}}</div>
+    <div>{{data.title}}</div>
+    <div>{{data.content}}</div>
+	<button @click="updateData">수정</button>
+	<button @click="deleteData">삭제</button>
+	인덱스: {{index}}
+  </div>
+</template>
+
+<script>
+import data from '@/data';
+export default {
+  name: "Detail",
+  data(){
+	  const index=this.$route.params.contentId
+	 
+	  return{
+		  data:data[index],
+		  index:index
+	  }
+  },
+  methods:{
+	  deleteData(){
+		  console.dir(this.index);
+		  data.splice(this.index, 1);
+		  this.$router.push({
+		 		path:'/'
+		  });
+	  },
+
+	  updateData(){
+		  this.$router.push({
+			  name:'Create',
+			  params:{
+				  contentId:this.index
+			  }
+		  });
+	  }
+  }
+
+};
+</script>
+
+<style>
+</style>
